32de8322afdafeeb85575802608971b7cdbccf91
yaya <2080625962@qq.com>
2021-08-25 19:00:43 +0800

    '完成项目页面结构'

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,34 @@
 <template>
-  <div>
-
+  <div id="app">
+    <myHeader></myHeader>
+    <myGoods></myGoods>
+    <myFooter></myFooter>
+    <!-- <myCount></myCount> -->
   </div>
 </template>
 
 <script>
+import myHeader from "./components/MyHeader.vue";
+import myGoods from "./components/MyGoods.vue";
+import myFooter from "./components/MyFooter.vue";
 export default {
-  data () {
-    return {
-
-    }
+  data() {
+    return {};
   },
-
-  created () {
-
+  components: {
+    myHeader,
+    myGoods,
+    myFooter,
+    // myCount,
   },
+  created() {},
 
-  methods: {
-
-  }
-}
+  methods: {},
+};
 </script>
 
 <style scoped>
-
+#app{
+  padding-top: 45px;
+}
 </style>
